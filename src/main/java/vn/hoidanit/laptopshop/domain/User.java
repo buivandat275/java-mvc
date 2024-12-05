9c4cac8b5494d9c61e162c831e04aa0c74f4a9f9
buivandat275 <buivandat275@gmail.com>
2024-12-06 01:35:16 +0700

    Relationship

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,9 +1,14 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +21,23 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+
+    private String avatar;
+    //User many to one role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+                                    
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 
     public long getId() {
         return id;
@@ -69,19 +91,14 @@ public class User {
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
-    // toString method
+// toString method
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", fullName='" + fullName + '\'' +
-                ", address='" + address + '\'' +
-                ", phone='" + phone + '\'' +
-                '}';
+        return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
+    
+   
 
 }
